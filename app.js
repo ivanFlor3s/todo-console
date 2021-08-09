f9e19405740bae648aebbad1410235d9ce7c5581
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2021-08-08 22:48:02 -0300

    menu con pausa hasta opcion distinta de 0

--- app.js
+++ app.js
@@ -6,9 +6,20 @@ const { mostrarMenu, pausa } = require('./helpers/mensajes');
 console.clear();
 
 const main = async() => {
-    console.log('Hola mundo');
-    mostrarMenu()
-    //pausa()
+    
+    let opt = ''
+    do {
+
+        opt = await mostrarMenu();
+        console.log({opt})
+        
+        
+        if ( opt !== '0')  await pausa();
+
+    } while (opt !== '0');
+     
+
+    
 }
 
 main();
--- helpers/mensajes.js
+++ helpers/mensajes.js
@@ -1,55 +1,60 @@
+const { resolve } = require("path");
 
-require('colors');
+require("colors");
 
-const mostrarMenu = ()=> {
+
+
+
+const mostrarMenu = () => {
+  return new Promise((resolve) => {
     console.clear();
-    console.log('=================================='.green);
-    console.log('===== Seleccione una opcion ======'.rainbow);
-    console.log('==================================\n'.green);
 
-    console.log(`${'1'.green}. Crear tareas`);
-    console.log(`${'2'.green}. Listar tareas`);
-    console.log(`${'3'.green}. Listar tareas completadas`);
-    console.log(`${'4'.green}. Listar tareas pendientes`);
-    console.log(`${'5'.green}. Completar tarea(s)`);
-    console.log(`${'6'.green}. Borrar tarea`); 
-    console.log(`${'0'.green}. Salir \n`);
+    console.log("==================================".green);
+    console.log("===== Seleccione una opcion ======".rainbow);
+    console.log("==================================\n".green);
 
-    //Preparo interfaz
-    //1 Creo interfaz
-    const readline = require('readline').createInterface({
+    console.log(`${"1".green}. Crear tareas`);
+    console.log(`${"2".green}. Listar tareas`);
+    console.log(`${"3".green}. Listar tareas completadas`);
+    console.log(`${"4".green}. Listar tareas pendientes`);
+    console.log(`${"5".green}. Completar tarea(s)`);
+    console.log(`${"6".green}. Borrar tarea`);
+    console.log(`${"0".green}. Salir \n`);
+
+    const READLINE = require("readline").createInterface({
         input: process.stdin,
-        output: process.stdout
-    })
+        output: process.stdout,
+      });
 
     //2 Uso la interfaz
-    readline.question('Seleccione una opcion: ', (opcion)=>{
-        console.log(opcion);
-        //Cierro el readloine para que no espere mas
-        readline.close();
-
-    })
-
-
-}
-
+    READLINE.question("Seleccione una opcion: ", (opcion) => {
+      //Cierro el readloine para que no espere mas
+      resolve(opcion);
+      READLINE.close();
+    });
+  });
+};
 
 //Funcion para pausar la consola
-const pausa = ()=>{
-    const readline = require('readline').createInterface({
+const pausa = () => {
+  return new Promise( resolve => {
+
+    const READLINE = require("readline").createInterface({
         input: process.stdin,
-        output: process.stdout
-    })
-    
+        output: process.stdout,
+      });
+      READLINE.question(
+      `\nPresione ${"ENTER".yellow} para continuar\n`,
+      (opcion) => {
+          READLINE.close();
+          resolve()
+      }
+    );
+  })
 
-    readline.question(`\nPresione ${'ENTER'.green} para continuar\n`, (opcion)=>{
-       
-        readline.close();
-
-    })
-}
+};
 
 module.exports = {
-    mostrarMenu,
-    pausa
-}
+  mostrarMenu,
+  pausa,
+};
